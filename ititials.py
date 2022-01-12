df38c447b64bfe20aa864993b4e936e5ef398398
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2022-01-12 16:33:16 -0500

    Create ititials.py

---
+++ ititials.py
@@ -0,0 +1,21 @@
+#Lauren Johnson
+#I love coffee
+firstInitial = """
+L
+L
+L
+L
+L
+L
+L L L L L
+"""
+lastInitial = """
+J J J J J
+    J
+    J
+    J
+J   J
+J   J
+  J J
+"""
+print(firstInitial + lastInitial)
